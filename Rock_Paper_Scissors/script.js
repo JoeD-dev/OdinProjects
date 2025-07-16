fc46248222eb652e46008b16fbebe718467dfaab
JoeD-dev <jd-mail@tutamail.com>
2025-07-16 11:13:10 +0300

    fix:Rock_Paper_Scissors: fix  bug with wrong round counting.
    fix bug whereround count incremented despite invalid user input

--- Rock_Paper_Scissors/script.js
+++ Rock_Paper_Scissors/script.js
@@ -11,39 +11,33 @@ let computerScore = 0;
 
 
 function newRound () {
-    if (humanAnswer === 'Rock' 
-        || humanAnswer === 'Paper' 
-        || humanAnswer === 'Scissors') {
-        if (humanAnswer === computerAnswer) {
-            alert(`
-                 Your choise is a ${humanAnswer}
-                 Computer choise is a ${computerAnswer}
-                 It's a draw!`);
-        }else if (humanAnswer === 'Rock' && computerAnswer === 'Scissors' 
-                  || humanAnswer === 'Paper' && computerAnswer === 'Rock' 
-                  || humanAnswer === 'Scissors' && computerAnswer === 'Paper') {
-                    alert(`
-                         Your choise is a ${humanAnswer}
-                         Computer choise is a ${computerAnswer}
-                         You win!`);
-                    humanScore++;
-        }else if (computerAnswer === 'Rock' && humanAnswer === 'Scissors' 
-                  || computerAnswer === 'Paper' && humanAnswer === 'Rock' 
-                  || computerAnswer === 'Scissors' && humanAnswer === 'Paper') {
-                    alert(`
-                         Your choise is a ${humanAnswer}
-                         Computer choise is a ${computerAnswer}
-                         You lose!`);
-                    computerScore++;
-        } else {
-            alert('Unexpected programm error')
-        };
-    }else{
-        alert('Your choise is wrong.Please try again')
+    if (humanAnswer === computerAnswer) {
+        alert(`
+             Your choise is a ${humanAnswer}
+             Computer choise is a ${computerAnswer}
+             It's a draw!`);
+    }else if (humanAnswer === 'Rock' && computerAnswer === 'Scissors' 
+              || humanAnswer === 'Paper' && computerAnswer === 'Rock' 
+              || humanAnswer === 'Scissors' && computerAnswer === 'Paper') {
+                alert(`
+                     Your choise is a ${humanAnswer}
+                     Computer choise is a ${computerAnswer}
+                     You win!`);
+                humanScore++;
+    }else if (computerAnswer === 'Rock' && humanAnswer === 'Scissors' 
+              || computerAnswer === 'Paper' && humanAnswer === 'Rock' 
+              || computerAnswer === 'Scissors' && humanAnswer === 'Paper') {
+                alert(`
+                     Your choise is a ${humanAnswer}
+                     Computer choise is a ${computerAnswer}
+                     You lose!`);
+                computerScore++;
+    } else {
+        alert('Unexpected programm error')
     };
 };
 
-while (counter <= 5) {
+while (counter < 5) {
     humanAnswer = prompt(`
         What your choise?
         Print: 'Rock', 'Paper' or 'Scissors'`, 'No answer');
@@ -55,9 +49,14 @@ while (counter <= 5) {
     
     if (humanAnswer === 'No answer' || humanAnswer === null) {
         break;
+    }else if (humanAnswer === 'Rock' 
+                || humanAnswer === 'Paper' 
+                || humanAnswer === 'Scissors') {
+                    newRound();
+                    counter++;
+    }else{
+        alert('Your choise is wrong.Please try again')
     };
-    newRound();
-    counter++;
 };
 
 if (counter > 0 && humanScore < computerScore) {
